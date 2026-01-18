195adf9e15536194b8c1e44192d48cb48f9a1470
SirDank <52797753+SirDank@users.noreply.github.com>
2026-01-18 18:52:25 +0530

    Update MainMenuFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import net.kdt.pojavlaunch.CustomControlsActivity;
